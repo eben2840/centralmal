4aa49636bca58161acd3b7cceb27dacb54999ebf
eben2840 <ebenmills200@gmail.com>
2022-10-14 04:10:05 +0000

    first oush

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, redirect, render_template, url_for,request
+
+
+
+
+app=Flask(__name__)
+app.config['SECRET_KEY']= 'ADKADKFJ'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+db = SQLAlchemy(app)
+
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    #DEBUG is SET to TRUE. CHANGE FOR PROD
+    app.run(port=5000,debug=True)

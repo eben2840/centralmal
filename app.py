bf2637bd86de92dbd81b22edca47fe54ecf757ec
eben2840 <ebenmills200@gmail.com>
2022-10-14 04:18:23 +0000

    first oush

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/')
 def home():
     return render_template('index.html')
 
